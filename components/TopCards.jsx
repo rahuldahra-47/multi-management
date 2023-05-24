7065e59d99faca70a29ca4c01ccc2dec5c64157c
Rahul Dahra <112661766+rahuldahra-47@users.noreply.github.com>
2023-05-24 14:20:27 +0530

    Update TopCards.jsx

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -27,7 +27,7 @@ const TopCards = () => {
         </div>
         <div className='lg:col-span-2 col-span-1 bg-gradient-to-r from-[#4568dc]/20 to-[#b06ab3]/20 flex justify-between w-full border p-4 rounded-lg'>
             <div className='flex flex-col w-full pb-4'>
-                <p className='text-2xl font-bold'></p>
+                <p className='text-2xl font-bold'>8</p>
                 <p className='text-gray-600'>Books Issued</p>
             </div>
             <p className=' flex justify-center items-center p-2 rounded-lg'>
